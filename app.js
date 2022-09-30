45be90d0044715d58a9598b0ce6ed8420bd10ffc
Abdulhakeem <70843998+ennas-de@users.noreply.github.com>
2022-09-30 10:41:54 +0100

    Add /name route

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// console.log(process.env.MESSAGE_STYLE);
+
+// app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+// root level middleware // it applies to all routes
+const middleware = (req, res, next) => {
+  console.log(req.method);
+  console.log(req.path);
+  console.log(req.ip);
+  next();
+};
+
+// console.log(req.path);
+
 app.get("/", (req, res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
+  res.send("Hello Abdulhakeem");
+});
+
+app.get("/json", middleware, (req, res) => {
+  req.MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+
+  if (req.MESSAGE_STYLE == "uppercase") {
     return res.json({
       message: "HELLO JSON",
     });
@@ -20,9 +39,38 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/json", (req, res) => {
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    console.log(req.params);
+    next();
+  },
+  (req, res) => {
+    return res.json({
+      time: req.time,
+    });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  //   console.log(req.params);
+  const word = req.params.word;
   res.json({
-    message: "Hello json",
+    echo: word,
+    // echo: req.params.word,
+  });
+});
+
+app.get("/name", (req, res) => {
+  console.log(req.query);
+
+  const { firstname, lastname } = req.query;
+  // const firstname = req.query.firstname;
+  // const lastname = req.query.lastname;
+
+  res.json({
+    name: `${firstname} ${lastname}`,
   });
 });
 
